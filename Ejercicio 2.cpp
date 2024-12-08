b71ce94322a548b918a0c414890a13c4df8bd797
PeterYelaR <122657973+PeterYelaR@users.noreply.github.com>
2024-12-08 00:10:14 -0500

    Update and rename tarea punto 2.cpp to Ejercicio 2.cpp

--- Ejercicio 2.cpp
+++ Ejercicio 2.cpp
@@ -1,6 +1,6 @@
-//2) Escribe un programa que solicite al usuario ingresar 10 números enteros entre 1 y 5 y los 
-//almacene en un vector. Usa un bucle para contar cuántas veces aparece cada 
-//número en el vector (del 1 al 5) y muestra el resultado.
+//2) Escribe un programa que solicite al usuario ingresar 10 nÃºmeros enteros entre 1 y 5 y los 
+//almacene en un vector. Usa un bucle para contar cuÃ¡ntas veces aparece cada 
+//nÃºmero en el vector (del 1 al 5) y muestra el resultado.
 #include <iostream>
 using namespace std;
 int main (){
